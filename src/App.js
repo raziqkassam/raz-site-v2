374bb2156bca1b3a53b68f2f53e5b3ad770df144
Raziq Kassam <raziq.kassam@gmail.com>
2024-11-10 19:26:48 -0700

    Added skills section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
+import Skills from "./components/Skills";
 import { Projects } from "./components/Experience";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
@@ -16,7 +16,7 @@ function App() {
       <Banner />
       <About />
       <Projects />
-      {/* <Skills /> */}
+      <Skills />
       {/* <Contact /> */}
       <Footer />
     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,66 +1,74 @@
+import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import webdev from "../assets/img/webdev.svg";
-import banner from "../assets/img/banner-bg.png"
-import styled from "styled-components";
+import styled from 'styled-components';
+
+import webdev from '../assets/img/skills/webdev.svg'; // Adjust the import based on your file structure
+import data from '../assets/img/skills/data.svg'; // Adjust the import based on your file structure
+import graphic from '../assets/img/skills/graphic.svg'; // Adjust the import based on your file structure
+import research from '../assets/img/skills/research.svg'; // Adjust the import based on your file structure
+import software from '../assets/img/skills/software.svg'; // Adjust the import based on your file structure
+import cad from '../assets/img/skills/cad.svg'; // Adjust the import based on your file structure
 
 const SkillImage = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 60px;
+  height: 60px;
 `;
 
-export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 4
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2
+  }
+};
 
+const Skills = () => {
   return (
-    <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={false} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <SkillImage src={webdev} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <SkillImage src={webdev} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <SkillImage src={webdev} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <SkillImage src={webdev} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <img className="background-image-left" src={banner} alt="Image" />
-    </section>
-  )
-}
+    <div className="skill col-12">
+      <div className="skill-bx wow zoomIn">
+        <h2>Skills</h2>
+        {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br /> Lorem Ipsum has been the industry's standard dummy text.</p> */}
+        <Carousel responsive={responsive} infinite={false} className="owl-carousel owl-theme skill-slider">
+          <div className="item">
+            <SkillImage src={software} alt="Web Development" />
+            <h5>Software Development</h5>
+          </div>
+          <div className="item">
+            <SkillImage src={webdev} alt="Brand Identity" />
+            <h5>Web Development</h5>
+          </div>
+          <div className="item">
+            <SkillImage src={cad} alt="Logo Design" />
+            <h5>CAD Design</h5>
+          </div>
+          <div className="item">
+            <SkillImage src={research} alt="Web Development" />
+            <h5>User Research</h5>
+          </div>
+          <div className="item">
+            <SkillImage src={data} alt="Web Development" />
+            <h5>Data Analytics</h5>
+          </div>
+          <div className="item">
+            <SkillImage src={graphic} alt="Web Development" />
+            <h5>Graphic Design</h5>
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
