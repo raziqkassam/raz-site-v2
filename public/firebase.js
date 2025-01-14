8f577e12f6d6c24cdfd75f902b8a9a89ebc70f7d
Raziq Kassam <raziq.kassam@gmail.com>
2025-01-14 14:13:02 -0700

    Updated API restrictions, security

--- public/firebase.js
+++ public/firebase.js
@@ -1,22 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJKU3DQieH-DyPjxPrg2-cFVC6s77rdZY",
-  authDomain: "raz-website.firebaseapp.com",
-  databaseURL: "https://raz-website-default-rtdb.firebaseio.com",
-  projectId: "raz-website",
-  storageBucket: "raz-website.appspot.com",
-  messagingSenderId: "404190164382",
-  appId: "1:404190164382:web:6fb6d2d1810335d225d28f",
-  measurementId: "G-JMSY069DWZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+console.log("Firebase initialized:", app);
+console.log("Firebase Analytics initialized:", analytics);
